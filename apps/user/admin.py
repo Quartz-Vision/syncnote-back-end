ae56d99d4aeca63fc945b045564e112c0cc410f6
Lia Va <mistickusya.2012@gmail.com>
2021-10-14 14:00:13 +0300

    fix: add password to the admin

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,6 +1,28 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-admin.site.register(User)
+
+class UserAdminForm(forms.ModelForm):
+    password = forms.CharField(max_length=128, widget=forms.PasswordInput, required=False)
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def save(self, commit=True):
+        password = self.data.get('password')
+        ret = super(UserAdminForm, self).save(commit)
+
+        if password:
+            self.instance.set_password(password)
+            self.instance.save()
+
+        return ret
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    form = UserAdminForm
