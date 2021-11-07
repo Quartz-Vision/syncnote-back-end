aa22f3bd396ba9fd3b5f103c9f24c2b782bbdfa8
Lia Va <mistickusya.2012@gmail.com>
2021-11-07 14:01:56 +0200

    feat(user): add data size fields

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,11 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from apps.common.models import UUIDModel
 
 
 class User(AbstractUser, UUIDModel):
-    """
-    User model with e-mail as a username
-    """
-    pass
+    used_data_size = models.IntegerField(
+        verbose_name=_('used data size')
+    )
+    max_data_size = models.IntegerField(
+        default=1048576,
+        verbose_name=_('allowed data size')
+    )
+    lang = models.CharField(
+        max_length=10,
+        default='en-us',
+        verbose_name=_('language')
+    )
